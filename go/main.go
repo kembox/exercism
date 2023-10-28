4aeb5114096e0685299dedc6138a12bf7892cd51
t13ag <tgnuadm@outlook.com>
2023-10-28 12:00:42 +0700

    go pangram

--- go/main.go
+++ go/main.go
@@ -4,42 +4,9 @@ import (
 	"fmt"
 )
 
-func SumInts(m map[string]int64) int64 {
-	var s int64
-	for _, v := range m {
-		s += v
-	}
-	return s
-}
-
-func SumFloats(m map[string]float64) float64 {
-	var s float64
-	for _, v := range m {
-		s += v
-	}
-	return s
-}
-
-func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
-	var s V
-	for _, v := range m {
-		s += v
-	}
-	return s
-}
-
 func main() {
-	fmt.Println("keep trying")
-	ints := map[string]int64{
-		"first":  34,
-		"second": 12,
-	}
-
-	floats := map[string]float64{
-		"first":  35.98,
-		"second": 26.99,
-	}
-
-	fmt.Printf("Non-generic sums: %v and %v\n", SumInts(ints), SumFloats(floats))
-	fmt.Printf("Non-generic sums: %v and %v\n", SumIntsOrFloats(ints), SumIntsOrFloats(floats))
+	c := fmt.Sprintf("%c", 65)
+	fmt.Printf("%T\n", c)
+	fmt.Println(string('A' + 2))
+	fmt.Printf("%T\n", string('A'+1))
 }
